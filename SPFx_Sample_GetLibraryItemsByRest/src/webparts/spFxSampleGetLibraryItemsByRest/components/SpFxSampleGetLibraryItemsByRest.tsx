3da896b41693fd56b8971fb3450098646b6b92b0
naoki_katsumi <naoki_katsumi@2fc-group.jp>
2018-07-06 16:18:40 +0900

    Update component and container

--- SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/components/SpFxSampleGetLibraryItemsByRest.tsx
+++ SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/components/SpFxSampleGetLibraryItemsByRest.tsx
@@ -3,12 +3,14 @@ import styles from './SpFxSampleGetLibraryItemsByRest.module.scss';
 import { ISPFxType } from '../containers/SPFxContainer';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+import ListItems from './ListItems';
+
 export default class SpFxSampleGetLibraryItemsByRest extends React.Component<ISPFxType, {}> {
   public render(): React.ReactElement<ISPFxType> {
     return (
       <div className={ styles.spFxSampleGetLibraryItemsByRest }>
         <div className={ styles.container }>
-        <div className={ styles.row }>
+          <div className={ styles.row }>
             <div className={ styles.column }>
               <span className={ styles.title }>Welcome to SharePoint!</span>
               <p className={ styles.subTitle }>{escape(this.props.displayName)} {escape(this.props.loginName)}</p>
@@ -17,17 +19,34 @@ export default class SpFxSampleGetLibraryItemsByRest extends React.Component<ISP
                 <span className={ styles.label }>Learn more</span>
               </a>
             </div>
-            <div>
-              <p className={ styles.description }>Input title : {escape(this.props.title)}</p>
+          </div>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <p className={ styles.description }>Input LibraryName : {escape(this.props.title)}</p>
               <input type="text" value={this.props.title} onChange={this.handleChange.bind(this)} />
+              <p>
+                <a className={ styles.button } onClick={this.handleClick.bind(this)}>
+                  <span className={ styles.label }>Get Library Items</span>
+                </a>
+              </p>
+              <p>
+              <ListItems listItems={this.props.listItems} />
+              </p>
             </div>
           </div>
         </div>
       </div>
     );
   }
+  private handleClick(event: React.FormEvent<HTMLInputElement>){
+    this.props.getLibraryItems(this.props.spHttpClient, this.props.currentWebUrl, this.props.title);
+  }
 
   private handleChange(event: React.FormEvent<HTMLInputElement>){
     this.props.updateTitle(event.currentTarget.value);
+  }
+
+  private componentDidMount() {
+    this.props.getLibraryItems(this.props.spHttpClient, this.props.currentWebUrl, this.props.title);
   }
 }
--- SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/containers/SPFxContainer.ts
+++ SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/containers/SPFxContainer.ts
@@ -4,20 +4,25 @@ import { connect } from 'react-redux';
 import { State, ISPFxProps } from '../state/SPFxState';
 import App from '../components/SpFxSampleGetLibraryItemsByRest';
 import * as Actions from '../actions/SPFxActionCreators';
+import { SPHttpClient } from '@microsoft/sp-http';
 
+import { getLibraryItems } from '../api/getLibraryItems';
 
 export interface IConnectedState {
   title: string;
+  listItems: any[];
 }
 export interface IConnectedDispatch {
   updateTitle: (title: string) => void;
+  getLibraryItems: (spHttpClient: SPHttpClient, currentWebUrl: string, libraryName: string) => void;
 }
 export type ISPFxType = ISPFxProps & IConnectedDispatch & IConnectedState;
 
 //Map the application state to the properties of the Components. Making them available in this.props inside the component.
 export function mapStateToProps(state: State, ownProps: ISPFxProps): IConnectedState {
   return {
-    title: state.title
+    title: state.title,
+    listItems: state.listItems
   };
 }
 
@@ -25,7 +30,10 @@ export function mapStateToProps(state: State, ownProps: ISPFxProps): IConnectedS
 export const mapDispatchToProps = (dispatch: Dispatch<State>): IConnectedDispatch => ({
   updateTitle: (title: string) => {
     dispatch(Actions.updateTitle(title));
-  }
+  },
+  getLibraryItems: (spHttpClient: SPHttpClient, currentWebUrl: string, libraryName: string) => {
+    dispatch(getLibraryItems(spHttpClient, currentWebUrl, libraryName));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
