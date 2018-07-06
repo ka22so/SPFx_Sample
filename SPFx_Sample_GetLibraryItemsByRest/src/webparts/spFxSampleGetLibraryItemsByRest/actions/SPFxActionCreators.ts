9fb917053843706dac8d025b22c3e35ed4fa39bd
naoki_katsumi <naoki_katsumi@2fc-group.jp>
2018-07-06 16:14:21 +0900

    Add actions

--- SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/actions/SPFxActionCreators.ts
+++ SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/actions/SPFxActionCreators.ts
@@ -6,3 +6,17 @@ export const updateTitle = (title: string): Action => ({
   payload: title
 });
 
+//Each AJAX request ideally has 3 actions: request, success and error.
+//Actions for getLibraryItems
+export const getLibraryItemsRequest = (): Action => ({
+  type: ActionTypes.GET_LIBRARYITEMS_REQUEST
+});
+export const getLibraryItemsSuccess = (lists: string[]): Action => ({
+  type: ActionTypes.GET_LIBRARYITEMS_SUCCESS,
+  payload: lists
+});
+export const getLibraryItemsError = (error: Error): Action => ({
+  type: ActionTypes.GET_LIBRARYITEMS_ERROR,
+  payload: error.message
+});
+
--- SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/actions/SPFxActionTypes.ts
+++ SPFx_Sample_GetLibraryItemsByRest/src/webparts/spFxSampleGetLibraryItemsByRest/actions/SPFxActionTypes.ts
@@ -1,6 +1,12 @@
 export enum ActionTypes {
-  UPDATE_TITLE
+  UPDATE_TITLE,
+  GET_LIBRARYITEMS_REQUEST,
+  GET_LIBRARYITEMS_SUCCESS,
+  GET_LIBRARYITEMS_ERROR
 }
 
 export type Action =
-  { type: ActionTypes.UPDATE_TITLE, payload: string };
+  { type: ActionTypes.UPDATE_TITLE, payload: string } |
+  { type: ActionTypes.GET_LIBRARYITEMS_REQUEST } |
+  { type: ActionTypes.GET_LIBRARYITEMS_SUCCESS, payload: string[] } |
+  { type: ActionTypes.GET_LIBRARYITEMS_ERROR, payload: string };
